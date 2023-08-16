d7fcd2ea8c5b6a0d0bfc47624e703e1cb4d498ef
confidential-nt <youyoy689@gmail.com>
2023-08-16 15:43:59 +0900

    [feat] 헤더 기본 스타일링

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,15 +3,19 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <header className="w-full max-w-6xl">
+    <header className="w-full max-w-6xl border-b border-b-black py-2">
       <nav className="flex justify-center relative ">
         <div>
           <Link to="/">
-            <h1 className="text-center">K-TOUR Planner</h1>
+            <h1 className="text-center text-xl lg:text-2xl font-semibold">
+              K-TOUR Planner
+            </h1>
           </Link>
-          <p>여행하고 싶은 곳을 CLICK! 나만의 여행 계획을 세워요</p>
+          <p className="text-center text-sm lg:text-base font-light">
+            여행하고 싶은 곳을 CLICK! 나만의 여행 계획을 세워요
+          </p>
         </div>
-        <Link to="/" className="absolute top-0 right-0">
+        <Link to="/" className="absolute top-0 right-4 lg:right-0">
           로그인
         </Link>
       </nav>
