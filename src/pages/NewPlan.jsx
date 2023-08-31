49abff51e344812eb9506406ab13fd3df79fdfb7
confidential-nt <youyoy689@gmail.com>
2023-08-31 12:42:53 +0900

    [feat] plan detail 로 갔을 때 선택한 관광지 정보 받아오기

--- src/pages/NewPlan.jsx
+++ src/pages/NewPlan.jsx
@@ -1,18 +1,25 @@
 import React, { useEffect, useState } from "react";
-import { useLocation, Link } from "react-router-dom";
+import { useLocation, Link, useNavigate } from "react-router-dom";
 import Map from "../components/Map";
-import TourismLists from "../components/TourismLists"
+import TourismLists from "../components/TourismLists";
 import { usePublicDataApi } from "../context/PublicDataApiContext";
 
 export default function NewPlan() {
   const { state: city } = useLocation();
   const [attractions, setAttractions] = useState([]);
+  const [selectedAttractions, setSelectedAttractions] = useState([]);
   const { publicData } = usePublicDataApi();
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     // 실제에서는 city 값을 통해 api로 실제 해당 지역의 관광명소 데이터를 받아올 것.
     publicData.getAttractions(city.name).then(setAttractions);
   }, [publicData, city]);
+
+  const onUpdateMyAttractions = (attractions) => {
+    setSelectedAttractions(attractions);
+  };
 
   return (
     <>
@@ -27,10 +34,11 @@ export default function NewPlan() {
               level={7}
               draggable={true}
               zoomable={false}
-              cities={[...attractions]}
+              cities={attractions}
             />
             <TourismLists
-              cities={[...attractions]}
+              cities={attractions}
+              onUpdateMyAttractions={onUpdateMyAttractions}
             />
           </>
         )}
@@ -39,7 +47,17 @@ export default function NewPlan() {
         <Link to="/" className="mr-2">
           이전
         </Link>
-        <Link to="/plans/detail">선택완료</Link>
+        <button
+          onClick={() =>
+            navigate("/plans/detail", {
+              state: {
+                selectedAttractions,
+              },
+            })
+          }
+        >
+          선택완료
+        </button>
       </div>
     </>
   );
--- src/pages/PlanDetail.jsx
+++ src/pages/PlanDetail.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 export default function PlanDetail() {
+  const {
+    state: { selectedAttractions },
+  } = useLocation();
+  console.log(selectedAttractions);
   return <div>여기는 여행 상세 페이지</div>;
 }
