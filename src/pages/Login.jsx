bd00b38b0165a987352ef60b2e1dd9f6e554ad3d
confidential-nt <youyoy689@gmail.com>
2023-09-04 18:36:41 +0900

    [feat] 로그인 링크 걸기

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { SiNaver } from "react-icons/si";
 import { RiKakaoTalkFill } from "react-icons/ri";
+import { v4 as uuidv4 } from "uuid";
 
 export default function Login() {
+  console.log();
   return (
     <section className="w-full h-full max-w-6xl flex justify-center items-center">
       <h2 className="hidden">로그인페이지</h2>
@@ -11,7 +13,7 @@ export default function Login() {
           간편<strong>로그인</strong>
         </h2>
         <a
-          href="#none"
+          href={`https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=gJGsnAly8Am9C_SzgqNW&state=${uuidv4()}&redirect_uri=http://localhost:8080/oauth/naver`}
           className="flex items-center bg-naver text-white mb-3  relative after:w-full after:bg-black after:absolute after:h-full after:opacity-20 after:hidden hover:after:block"
         >
           <span className="flex justify-center items-center p-3 border-r basis-1/6 text-center">
@@ -22,7 +24,7 @@ export default function Login() {
           </span>
         </a>
         <a
-          href="#none"
+          href="https://kauth.kakao.com/oauth/authorize?client_id=20150f9bcb2ccc3b3b25b08b6a116e76&redirect_uri=http://localhost:8080/oauth/kakao&response_type=code"
           className="flex items-center bg-kakao relative after:w-full after:bg-black after:absolute after:h-full after:opacity-20 after:hidden hover:after:block"
         >
           <span className="flex justify-center items-center p-3 border-r basis-1/6 text-2xl">
